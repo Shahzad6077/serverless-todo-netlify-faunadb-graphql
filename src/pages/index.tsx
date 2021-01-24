bd026acb1c21bbc9ae55d2bebd17f5e3c9f14621
shahzad6077 <alim6077@gmail.com>
2021-01-24 22:53:04 +0500

    Add redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import React, { FC, useEffect } from "react"
 import { SEO } from "../Components"
-import { Link } from "gatsby"
+import { Link, navigate } from "gatsby"
 import netlifyIdentity from "netlify-identity-widget"
+import { useAuthContext } from "../Context/Auth"
 type Props = {}
 
 const Index: FC<Props> = () => {
+  const { isAuthenticated } = useAuthContext()
+
+  useEffect(() => {
+    typeof window !== "undefined" && isAuthenticated && navigate("/app")
+  }, [isAuthenticated])
   return (
     <div>
       <SEO title="Home" />
